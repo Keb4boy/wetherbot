636217a19a1b1016593c7a7f82058802c4296a13
keb4boy <Grecha_AI_2005@icloud.com>
2023-03-28 15:51:46 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # wetherbot
+
+This bot is made to find out the weather in any period of time. This facilitates travel planning and minimizes the risk of being hit by bad weather.
+___
+
+## Getting started
+
+To start working with this project
--- main.py
+++ main.py
@@ -11,29 +11,29 @@ from dotenv import load_dotenv
 import os
 
 
-
 load_dotenv()
-API_TOKEN=os.environ.get("API_TOKEN")
+API_TOKEN = os.environ.get("API_TOKEN")
+
 
 def get_keyboard() -> ReplyKeyboardMarkup:
-    # функция для создания клавиатуры
+    """Create keyboard with cities"""
     kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     kb.add(KeyboardButton("Moscow"), ("London"), ("Saint-Petersburg"), ("Paris"), ("Rome"))
     return kb
 
 def get_cmdcheck() -> ReplyKeyboardMarkup:
-    # функция для создания клавиатуры
+    """Create keyboard for command /check"""
     kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     kb.add(KeyboardButton("/check"))
     return kb
 
 def get_cmdstart() -> ReplyKeyboardMarkup:
-    # функция для создания клавиатуры
+    """Create keyboard for command /start"""
     kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     kb.add(KeyboardButton("/start"))
     return kb
 def get_params(city, start, end):
-    # функция для создания словаря
+    """Generating params for weather API"""
     dates = []
     geolocator = Nominatim(user_agent="weather_bot")
 
@@ -55,9 +55,11 @@ def get_params(city, start, end):
     return dates
 
 async def fetch_result(session, params):
+    """Creates json file with weather values"""
     async with session.get('https://archive-api.open-meteo.com/v1/archive', params=params) as result:
         return await result.json()
 async def get_request(all_params):
+    """Creates asynchronous requests"""
     async with aiohttp.ClientSession() as session:
         requests = [fetch_result(session, params) for params in all_params]
         result = await asyncio.gather(*requests, return_exceptions=False)
@@ -66,7 +68,7 @@ async def get_request(all_params):
 
 
 def get_max(weath):
-    # функция для вычисления среднего значения максимальной температуры
+    """Sorts json file and return max value"""
     max_list = []
     for maximum in weath["daily"]['temperature_2m_max']:
         max_list.append(maximum)
@@ -74,19 +76,21 @@ def get_max(weath):
 
 
 def get_min(weath):
-    # функция для вычисления среднего значения минимальной температуры
+    """Sorts json file and return min value"""
     min_list = []
     for minimum in weath["daily"]['temperature_2m_min']:
         min_list.append(minimum)
     return min(min_list)
 
 def get_average(weath):
+    """Sorts json file and return average value"""
     ave_list = []
     for ave in weath["daily"]['temperature_2m_min']:
         ave_list.append(ave)
     return sum(ave_list) // len(ave_list)
 
 async def main(city, start, end):
+        """Makes lists with all values and sorts it"""
         minimum = []
         maximum = []
         average = []
@@ -107,7 +111,7 @@ storage = MemoryStorage()
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
-class group(StatesGroup):
+class Group(StatesGroup):
     city = State()
     start_date = State()
     end_date = State()
@@ -122,27 +126,27 @@ async def send_welcome(message: types.Message):
 async def cmd_get(message: types.Message):
 
     await message.reply("Для начала укажи город который тебя интересует", reply_markup=get_keyboard())
-    await group.city.set()
+    await Group.city.set()
 
-@dp.message_handler(state=group.city)
+@dp.message_handler(state=Group.city)
 async def get_city(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["city"] = message.text
 
     await message.reply("Напишите дату в формате 01-01 (месяц-день), с начала которой хотите получить погоду")
-    await group.next()
+    await Group.next()
 
 
-@dp.message_handler(state=group.start_date)
+@dp.message_handler(state=Group.start_date)
 async def get_startdate(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["start_date"] = message.text
 
     await message.reply("Напишите дату, которая является конечной")
-    await group.next()
+    await Group.next()
 
 
-@dp.message_handler(state=group.end_date)
+@dp.message_handler(state=Group.end_date)
 async def get_endate(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["end_date"] = message.text
